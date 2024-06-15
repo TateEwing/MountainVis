e298cd2e5650f8a2d47cc7097dc4577c5cf465d1
Tate Ewing <tateewing@Tates-MacBook-Air.local>
2024-06-15 10:57:02 -0700

    update app to use Mapbox

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def load_data():
 df = load_data()
 
 deck = pdk.Deck(
+    # api_keys={"mapbox": "pk.eyJ1IjoidGF0ZS1ld2luZyIsImEiOiJjbHhkbjNncW8wN3F1MnBwb284eDRjZTRsIn0.2L99AQ1SOkn06v99tCr3yQ"},
+    map_provider="mapbox",
+    map_style="mapbox://styles/mapbox/outdoors-v12",
     initial_view_state=pdk.ViewState(latitude=47.6061, longitude=-122.3328, zoom=11, bearing=0, pitch=45),
     layers=[
         pdk.Layer(
@@ -26,13 +29,14 @@ deck = pdk.Deck(
     ],
 )
 
-st.title("My Outdoor Activity :man-mountain-biking: :snowboarder: :snow_capped_mountain:")
+st.title("Tate's Outdoor Activity :man-mountain-biking: :snowboarder: :snow_capped_mountain:")
 st.pydeck_chart(deck)
 st.write('''
-This is a quick project I put together to refresh my skills working with data, and to learn about :red[Streamlit].
-My GPS location is represented in a cloud of points, with elevation gain within each activity represented by the color gradient.
+This is a project I put together to have some fun with Mapbox and work with my own GPX data.
+         
+I've spent a lot of time using mapping software to help find spots for these adventures, so it's cool to see what's behind the scenes.
 
 The data are from :orange[Strava], an app I use to track my :green[outdoor activities] :camping:.
-Scroll to the east along I-90 to see some of my regular spots.
+Scroll to the east along I-90 to see some of my regular spots. 
 Most of my activities are around Seattle, but if you zoom out you'll see some places I've traveled to :car: :smile:.
 ''')
